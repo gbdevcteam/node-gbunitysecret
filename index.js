8bf4a4b4065c2f4fb38718e2d97fd09765e79f33
zunmaxiake <bao6832701@163.com>
2016-12-23 16:46:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-/*function： parseHmacSha1Token
- *var hmac = require('crypto').createHmac('sha1', privateKey);
- *hmac.update(publicKey);
- *var token= encodeURIComponent(hmac.digest('base64'));
- */  
+var crypto = require('crypto');
 
 var getToken = {};
 getToken.parseHmacSha1Token = function(publicKey, privateKey){
-  var crypto = require('crypto');
   var hmac = crypto.createHmac('sha1', privateKey);
   hmac.update(publicKey);
   var token= encodeURIComponent(hmac.digest('base64'));
   return token;
-}
+};
 
 module.exports = getToken;
 // module.exports = {
