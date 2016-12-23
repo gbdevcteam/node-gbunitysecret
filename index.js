93e5fda7a378773e2a547ba5e4f52436d0759c3c
zunmaxiake <bao6832701@163.com>
2016-12-23 16:34:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//function： parseHmacSha1Token
+/*function： parseHmacSha1Token
+ *var hmac = require('crypto').createHmac('sha1', privateKey);
+ *hmac.update(publicKey);
+ *var token= encodeURIComponent(hmac.digest('base64'));
+ */  
+
+var getToken = {};
+getToken.parseHmacSha1Token = function(publicKey, privateKey){
+  var crypto = require('crypto');
+  var hmac = crypto.createHmac('sha1', privateKey);
+  hmac.update(publicKey);
+  var token= encodeURIComponent(hmac.digest('base64'));
+  return token;
+}
+
+module.exports = getToken;
+// module.exports = {
+//   parseHmacSha1Token: function (publicKey, privateKey) {
+//       return parseHmacSha1Token(publicKey, privateKey);
+//   }
+// }
